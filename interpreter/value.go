50372ab2de93ffdaceb4c9936bcf1e520450a9ba
Tony BROYEZ <tonybroyez@Mac-001.lan>
2026-02-14 01:15:29 +0100

    refactor: extract callable value types

--- interpreter/value.go
+++ interpreter/value.go
@@ -3,8 +3,6 @@ package interpreter
 import (
 	"bytes"
 	"fmt"
-	"karl/ast"
-	"strings"
 )
 
 type ValueType string
@@ -216,41 +214,4 @@ func (s *Set) Inspect() string {
 	}
 	out.WriteString("}")
 	return out.String()
-}
-
-type Function struct {
-	Params []ast.Pattern
-	Body   ast.Expression
-	Env    *Environment
-}
-
-func (f *Function) Type() ValueType { return FUNC }
-func (f *Function) Inspect() string { return "<function>" }
-
-type BuiltinFunction func(e *Evaluator, args []Value) (Value, error)
-
-type Builtin struct {
-	Name string
-	Fn   BuiltinFunction
-}
-
-func (b *Builtin) Type() ValueType { return BUILTIN }
-func (b *Builtin) Inspect() string { return "<builtin " + b.Name + ">" }
-
-type Partial struct {
-	Target Value
-	Args   []Value
-}
-
-func (p *Partial) Type() ValueType { return PARTIAL }
-func (p *Partial) Inspect() string {
-	parts := []string{}
-	for _, arg := range p.Args {
-		if arg == nil {
-			parts = append(parts, "_")
-		} else {
-			parts = append(parts, arg.Inspect())
-		}
-	}
-	return "<partial (" + strings.Join(parts, ", ") + ")>"
 }
