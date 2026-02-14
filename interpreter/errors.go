f91756202efcb94d1d17822db34bc76e654441f9
Tony BROYEZ <tonybroyez@Mac-001.lan>
2026-02-14 11:47:54 +0100

    refactor: isolate AST token extraction for errors

--- interpreter/errors.go
+++ interpreter/errors.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"strings"
 
-	"karl/ast"
 	"karl/token"
 )
 
@@ -95,83 +94,4 @@ func (e *UnhandledTaskError) Error() string {
 		return "unhandled task failure"
 	}
 	return "unhandled task failures:\n\n" + strings.Join(e.Messages, "\n\n")
-}
-
-func tokenFromNode(node ast.Node) *token.Token {
-	switch n := node.(type) {
-	case *ast.LetStatement:
-		return &n.Token
-	case *ast.ExpressionStatement:
-		return &n.Token
-	case *ast.Identifier:
-		return &n.Token
-	case *ast.Placeholder:
-		return &n.Token
-	case *ast.IntegerLiteral:
-		return &n.Token
-	case *ast.FloatLiteral:
-		return &n.Token
-	case *ast.StringLiteral:
-		return &n.Token
-	case *ast.CharLiteral:
-		return &n.Token
-	case *ast.BooleanLiteral:
-		return &n.Token
-	case *ast.NullLiteral:
-		return &n.Token
-	case *ast.UnitLiteral:
-		return &n.Token
-	case *ast.PrefixExpression:
-		return &n.Token
-	case *ast.InfixExpression:
-		return &n.Token
-	case *ast.AssignExpression:
-		return &n.Token
-	case *ast.PostfixExpression:
-		return &n.Token
-	case *ast.AwaitExpression:
-		return &n.Token
-	case *ast.ImportExpression:
-		return &n.Token
-	case *ast.RecoverExpression:
-		return &n.Token
-	case *ast.IfExpression:
-		return &n.Token
-	case *ast.BlockExpression:
-		return &n.Token
-	case *ast.MatchExpression:
-		return &n.Token
-	case *ast.ForExpression:
-		return &n.Token
-	case *ast.LambdaExpression:
-		return &n.Token
-	case *ast.CallExpression:
-		return &n.Token
-	case *ast.MemberExpression:
-		return &n.Token
-	case *ast.IndexExpression:
-		return &n.Token
-	case *ast.SliceExpression:
-		return &n.Token
-	case *ast.ArrayLiteral:
-		return &n.Token
-	case *ast.ObjectLiteral:
-		return &n.Token
-	case *ast.StructInitExpression:
-		return &n.Token
-	case *ast.RangeExpression:
-		return &n.Token
-	case *ast.QueryExpression:
-		return &n.Token
-	case *ast.RaceExpression:
-		return &n.Token
-	case *ast.SpawnExpression:
-		return &n.Token
-	case *ast.BreakExpression:
-		return &n.Token
-	case *ast.ContinueExpression:
-		return &n.Token
-	default:
-		return nil
-	}
 }
